96c28cf305dfedd69ea5b09206eac75f278b0afe
FrissBrot <timoweber2006@gmail.com>
2024-06-30 20:49:07 +0200

    added transaction add form

--- functions.py
+++ functions.py
@@ -1,11 +1,5 @@
 import dbfunctions as db
 
-def get_bank():
-    query = ('SELECT * from bank')
-    answer = db.executeQuery(query)
-    print(answer)
-    return answer
-
 def get_transactions():
     query = ('SELECT date, public."transactionType".name AS transactionType,'
              'public."transactionCategory".name AS transactionCategory, '
@@ -14,6 +8,41 @@ def get_transactions():
              'INNER JOIN public."transactionType" '
              'ON public."transactionType".id = transaction.fk_type '
              'INNER JOIN public."transactionCategory" '
-             'ON public."transactionCategory".id = transaction.fk_category ')
+             'ON public."transactionCategory".id = transaction.fk_category;')
     answer = db.executeQuery(query)
     return answer
+
+def get_transactionsTypes():
+    query = 'SELECT name FROM public."transactionType";'
+    answer_raw = db.executeQuery(query)
+
+    answer = []
+    for row in answer_raw:
+        answer.append(row[0])
+
+    return answer
+
+def get_transactionsCategories():
+    query = 'SELECT name FROM public."transactionCategory";'
+    answer_raw = db.executeQuery(query)
+
+    answer = []
+    for row in answer_raw:
+        answer.append(row[0])
+
+    return answer
+
+def get_transactionCategory_id(name):
+    query = ('SELECT id FROM public."transactionCategory" WHERE name = \'%s\';' % name)
+    answer = db.executeQuery(query)
+    return answer[0][0]
+
+def get_transactionType_id(name):
+    query = ('SELECT id FROM public."transactionType" WHERE name = \'%s\';' % name)
+    answer = db.executeQuery(query)
+    return answer[0][0]
+
+def add_transaction(user_id, type_id, category_id, bankAccount_id, currency_id, date, amount, description):
+    params = (user_id, type_id, category_id, bankAccount_id, currency_id, date, amount, description)
+    query = ('INSERT INTO public.transaction(fk_user, fk_type, fk_category, "fk_bankAccount", fk_currency, date, amount, description) VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\');' % params)
+    db.executeWithoutFetch(query)
--- main.py
+++ main.py
@@ -3,8 +3,27 @@ import pandas as pd
 import streamlit as st
 import functions as fn
 
+transactionTypes = fn.get_transactionsTypes()
+transactionCategories = fn.get_transactionsCategories()
 
 
+with st.form("Transaktion erfassen"):
+    st.markdown("### Transaktion erfassen")
+    form_transactionDate = st.date_input("Datum:", format="DD.MM.YYYY")
+    form_transactionType = st.selectbox("Typ:", (transactionTypes))
+    form_transactionCategory = st.selectbox("Kategorie:", (transactionCategories))
+    form_transactionAmount = st.number_input("Betrag in CHF")
+    form_transactionDescrition = st.text_input("Beschreibung")
+    submitted_add_transaction = st.form_submit_button("Transaktion erfassen")
+
+    if submitted_add_transaction:
+        category_id = fn.get_transactionCategory_id(form_transactionCategory)
+        type_id = fn.get_transactionType_id(form_transactionType)
+        fn.add_transaction(1, type_id, category_id, 3, 2, form_transactionDate, form_transactionAmount, form_transactionDescrition)
+
+
+
+# Transaction Table
 data= fn.get_transactions()
 
 columns = ["Datum", "Typ", "Kategorie", "Betrag", "Beschreibung"]
@@ -17,7 +36,7 @@ st.dataframe(
         "Betrag": st.column_config.NumberColumn(
             "Betrag",
             help="The price of the product in CHF",
-            format="%d CHF",
+            format="%.2f CHF",
         ),
 
     },
